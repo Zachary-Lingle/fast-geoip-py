2ab45401dadf612e9e06c24c7a9d2dfdde04f01f
Zachary-Lingle <102211401+Zachary-Lingle@users.noreply.github.com>
2022-05-14 15:00:44 +0800

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,6 @@ from urllib.parse import urlparse
 
 from search import search_ip
 
-data = {'result': 'this is a test'}
 host = ('localhost', 21700)
 
 
@@ -41,7 +40,7 @@ class Cache:
 
 class Resquest(BaseHTTPRequestHandler):
     timeout = 5
-    server_version = "Apache"  # 设置服务器返回的的响应头
+    server_version = "Apache"
     cache = Cache(10)
 
     def response_data(self, info: str):
@@ -50,7 +49,7 @@ class Resquest(BaseHTTPRequestHandler):
     def do_GET(self):
         querys = urlparse(self.path).query
         self.send_response(200)
-        self.send_header("Content-type", "text/json")  # 设置服务器响应头
+        self.send_header("Content-type", "text/json")
         self.end_headers()
         if querys:
             querys = querys.split('&')
@@ -66,32 +65,8 @@ class Resquest(BaseHTTPRequestHandler):
         else:
             self.response_data('Error')
 
-    # def do_POST(self):
-    #     path = self.path
-    #     print(path)
-    #     # 获取post提交的数据
-    #     datas = self.rfile.read(int(self.headers['content-length']))  # 固定格式，获取表单提交的数据
-    #     # datas = urllib.unquote(datas).decode("utf-8", 'ignore')
-    #
-    #     self.send_response(200)
-    #     self.send_header("Content-type", "text/html")  # 设置post时服务器的响应头
-    #     self.send_header("test", "This is post!")
-    #     self.end_headers()
-    #
-    #     html = '''<!DOCTYPE HTML>
-    #     <html>
-    #         <head>
-    #             <title>Post page</title>
-    #         </head>
-    #         <body>
-    #             Post Data:%s  <br />
-    #             Path:%s
-    #         </body>
-    #     </html>''' % (datas, self.path)
-    #     self.wfile.write(html.encode())  # 提交post数据时，服务器跳转并展示的页面内容
-
 
 if __name__ == '__main__':
     server = HTTPServer(host, Resquest)
-    print("Starting server, listen at: %s:%s" % host)
+    print("Starting fast-geoip-py server, listen at: %s:%s" % host)
     server.serve_forever()
